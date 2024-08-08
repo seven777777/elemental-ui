b786e851920918da0db5c75aff910f9f836b00e2
张敏 <zhangminh@digiwin.com>
2024-08-08 10:38:18 +0800

    feat: bem命名空间

---
+++ packages/components/icon/src/icon.vue
@@ -0,0 +1,11 @@
+<template>
+    <div :class="bem.b()">
+        <i :class="bem.be('box', 'inner')">123</i>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useNamespace } from '@elemental-ui/hooks';
+const bem = useNamespace('icon');
+</script>
+p
